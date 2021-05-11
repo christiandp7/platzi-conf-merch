5d1cce258b081922e7e79c4d6f39fe7c654b3cfc
Christian Diaz Portela <christiandp7@gmail.com>
2021-05-10 21:16:10 -0400

    settup webpack, setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 80,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: 'avoid',
+}
