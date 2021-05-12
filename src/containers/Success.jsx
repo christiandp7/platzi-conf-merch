2b12d409be4fafe946478a02d431465a1bdb2c50
Christian Diaz Portela <christiandp7@gmail.com>
2021-05-11 21:07:20 -0400

    Pages connected by Link, Include icons

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../styles/components/Success.css'
 
 const Success = () => {
   return (
-    <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Christian, gracias por tu pedido</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion</span>
+        <div className="Success-">
+          Google Maps integration
+        </div>
+      </div>
+    </div>
   )
 }
 
